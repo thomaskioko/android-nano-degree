16320008025a18020c85422494a482045227962d
kioko <kiokotomas@gmail.com>
2016-07-11 11:20:14 +0300

    Add Forecast fragment

--- Sunshine/app/src/main/java/com/thomaskioko/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/thomaskioko/sunshine/MainActivity.java
@@ -1,7 +1,9 @@
 package com.thomaskioko.sunshine;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.thomaskioko.sunshine.fragments.ForecastFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new ForecastFragment())
+                    .commit();
+        }
     }
 }
