1c0677c712d6bcb7ebcfc2202320c042b933bc58
kioko <kiokotomas@gmail.com>
2016-07-12 08:22:06 +0300

    Create detail fragment

---
+++ Sunshine/app/src/main/java/com/thomaskioko/sunshine/fragments/DetailFragment.java
@@ -0,0 +1,69 @@
+package com.thomaskioko.sunshine.fragments;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.thomaskioko.sunshine.R;
+import com.thomaskioko.sunshine.SettingsActivity;
+
+/**
+ *
+ */
+public class DetailFragment extends Fragment {
+
+
+    public DetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        Intent intent = getActivity().getIntent();
+
+        if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+            String passedData = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+            TextView detailWeather = (TextView) rootView.findViewById(R.id.detail_weather);
+            detailWeather.setText(passedData);
+        }
+        return rootView;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_detail_settings, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                startActivity(new Intent(getActivity(), SettingsActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
