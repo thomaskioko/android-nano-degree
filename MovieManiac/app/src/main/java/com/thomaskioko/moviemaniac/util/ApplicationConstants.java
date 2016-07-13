fbf23d0e50934ca201b945af66a1a9de6468dccc
kioko <kiokotomas@gmail.com>
2016-07-13 22:50:44 +0300

    Create class with constant variables.

---
+++ MovieManiac/app/src/main/java/com/thomaskioko/moviemaniac/util/ApplicationConstants.java
@@ -0,0 +1,32 @@
+package com.thomaskioko.moviemaniac.util;
+
+/**
+ * @author Thomas Kioko
+ */
+
+
+public class ApplicationConstants {
+
+    /**
+     * API Endpoint
+     */
+    public static final String END_POINT = "http://api.themoviedb.org/3/";
+    /**
+     * TMDB API
+     * To get one {@see href="https://www.themoviedb.org/account/signup"}
+     */
+    public static final String TMDB_API_KEY = "PUT_API_KEY_HERE";
+    /**
+     * Connection timeout duration
+     */
+    public static final int CONNECT_TIMEOUT = 60 * 1000;
+    /**
+     * Connection Read timeout duration
+     */
+    public static final int READ_TIMEOUT = 60 * 1000;
+    /**
+     * Connection write timeout duration
+     */
+    public static final int WRITE_TIMEOUT = 60 * 1000;
+
+}
