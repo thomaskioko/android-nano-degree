cce0c0cf3734e46bdc348692d22ff49cdb3701f9
kioko <kiokotomas@gmail.com>
2016-07-20 09:21:13 +0300

    Add location column names

--- Sunshine/app/src/main/java/com/thomaskioko/sunshine/data/WeatherContract.java
+++ Sunshine/app/src/main/java/com/thomaskioko/sunshine/data/WeatherContract.java
@@ -41,6 +41,13 @@ public class WeatherContract {
     public static final class LocationEntry implements BaseColumns {
         public static final String TABLE_NAME = "location";
 
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+
+        public static final String COLUMN_CITY_NAME = "city_name";
+
+        public static final String COLUMN_COORD_LONG = "coord_long";
+
+        public static final String COLUMN_COORD_LAT = "coord_lat";
     }
 
     /* Inner class that defines the table contents of the weather table */
