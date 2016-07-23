84b46b554dfa2331c901f938e1f64671c1a60a0d
Thomas Kioko <kiokotomas@gmail.com>
2016-07-23 17:49:16 +0300

    Load data using Loader

--- Sunshine/app/src/main/java/com/thomaskioko/sunshine/data/tasks/FetchWeatherTask.java
+++ Sunshine/app/src/main/java/com/thomaskioko/sunshine/data/tasks/FetchWeatherTask.java
@@ -1,68 +1,28 @@
 package com.thomaskioko.sunshine.data.tasks;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.AsyncTask;
-import android.widget.ArrayAdapter;
 
 import com.thomaskioko.sunshine.data.WeatherContract;
-import com.thomaskioko.sunshine.data.WeatherDbHelper;
 import com.thomaskioko.sunshine.net.HttpHelper;
 
 public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
 
-    private ArrayAdapter<String> mForecastAdapter;
     private final Context mContext;
-
-    public FetchWeatherTask(Context context, ArrayAdapter<String> forecastAdapter) {
-        mContext = context;
-        mForecastAdapter = forecastAdapter;
-    }
+    private String mLocation;
 
     /**
-     * Helper method to handle insertion of a new location in the weather database.
+     * Constructor
      *
-     * @param locationSetting The location string used to request updates from the server.
-     * @param cityName        A human-readable city name, e.g "Mountain View"
-     * @param lat             the latitude of the city
-     * @param lon             the longitude of the city
-     * @return the row ID of the added location.
+     * @param context  Application context
+     * @param location Set location
      */
-    public long addLocation(String locationSetting, String cityName, double lat, double lon) {
-        WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-        long locationId;
-
-
-        //Check if the location with the city name exists
-        Cursor cursor = mContext.getContentResolver().query(
-                WeatherContract.LocationEntry.CONTENT_URI,
-                new String[]{WeatherContract.LocationEntry._ID},
-                WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING + " = ? ",
-                new String[]{locationSetting},
-                null
-        );
-
-        assert cursor != null;
-        if (cursor.moveToFirst()) {
-            int locationIndex = cursor.getColumnIndex(WeatherContract.LocationEntry._ID);
-            locationId = cursor.getLong(locationIndex);
-        } else {
-
-            ContentValues contentValues = new ContentValues();
-            contentValues.put(WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING, locationSetting);
-            contentValues.put(WeatherContract.LocationEntry.COLUMN_CITY_NAME, cityName);
-            contentValues.put(WeatherContract.LocationEntry.COLUMN_COORD_LAT, lat);
-            contentValues.put(WeatherContract.LocationEntry.COLUMN_COORD_LONG, lon);
-
-            locationId = db.insert(WeatherContract.LocationEntry.TABLE_NAME, null, contentValues);
-        }
-        cursor.close();
-        return locationId;
+    public FetchWeatherTask(Context context, String location) {
+        mContext = context;
+        mLocation = location;
     }
-
 
     @Override
     protected String[] doInBackground(String... params) {
@@ -74,12 +34,15 @@ public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
 
     @Override
     protected void onPostExecute(String[] result) {
-        if (result != null && mForecastAdapter != null) {
-            mForecastAdapter.clear();
-            for (String dayForecastStr : result) {
-                mForecastAdapter.add(dayForecastStr);
-            }
-            // New data is back from the server.  Hooray!
-        }
+
+        // Sort order:  Ascending, by date.
+        String sortOrder = WeatherContract.WeatherEntry.COLUMN_DATE + " ASC";
+        Uri weatherForLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(
+                mLocation, System.currentTimeMillis());
+
+        Cursor cursor = mContext.getContentResolver().query(weatherForLocationUri,
+                null, null, null, sortOrder);
+
+        new ForecastAdapter(mContext, cursor, 0);
     }
 }
