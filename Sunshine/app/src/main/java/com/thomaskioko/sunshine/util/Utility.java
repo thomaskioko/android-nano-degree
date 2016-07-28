d71e0b55abc4a6da22e3193faff54b8b9d3d31d2
Thomas Kioko <kiokotomas@gmail.com>
2016-07-28 12:51:57 +0300

    Add method to format temperature

--- Sunshine/app/src/main/java/com/thomaskioko/sunshine/util/Utility.java
+++ Sunshine/app/src/main/java/com/thomaskioko/sunshine/util/Utility.java
@@ -47,6 +47,19 @@ public class Utility {
         return String.format(Locale.getDefault(), "%.0f", temp);
     }
 
+
+    public static String formatTemperature(Context context, double temperature) {
+        // Data stored in Celsius by default.  If user prefers to see in Fahrenheit, convert
+        // the values here.
+        String suffix = "\u00B0";
+        if (!isMetric(context)) {
+            temperature = (temperature * 1.8) + 32;
+        }
+
+        // For presentation, assume the user doesn't care about tenths of a degree.
+        return String.format(context.getString(R.string.format_temperature), temperature);
+    }
+
     /**
      * Method to format date
      *
