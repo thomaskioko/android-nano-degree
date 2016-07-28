448ec0b2277a62d2c3907c1fbc70ad40a7b8040b
Thomas Kioko <kiokotomas@gmail.com>
2016-07-28 12:49:59 +0300

    Pass URI in bundle

--- Sunshine/app/src/main/java/com/thomaskioko/sunshine/DetailActivity.java
+++ Sunshine/app/src/main/java/com/thomaskioko/sunshine/DetailActivity.java
@@ -18,8 +18,14 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         if (savedInstanceState == null) {
+            Bundle arguments = new Bundle();
+            arguments.putParcelable(DetailFragment.DETAIL_URI, getIntent().getData());
+
+            DetailFragment fragment = new DetailFragment();
+            fragment.setArguments(arguments);
+
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.weather_detail_container, new DetailFragment())
+                    .add(R.id.weather_detail_container, fragment)
                     .commit();
         }
     }
