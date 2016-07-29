9c5523b98a6ec1b1461abaaaa9fa0a2d73c63630
Thomas Kioko <kiokotomas@gmail.com>
2016-07-29 12:44:12 +0300

    Delete API key constant

--- MovieManiac/app/src/main/java/com/thomaskioko/moviemaniac/util/ApplicationConstants.java
+++ MovieManiac/app/src/main/java/com/thomaskioko/moviemaniac/util/ApplicationConstants.java
@@ -17,11 +17,6 @@ public class ApplicationConstants {
      */
     public static final String END_POINT = "http://api.themoviedb.org/3/movie/";
     /**
-     * TMDB API
-     * To get one {@see href="https://www.themoviedb.org/account/signup"}
-     */
-    public static final String TMDB_API_KEY = "df7ab6bd6119f184002f7065000d0fc8";
-    /**
      * Image base Url
      */
     public static final String TMDB_IMAGE_URL = "http://image.tmdb.org/t/p/";
