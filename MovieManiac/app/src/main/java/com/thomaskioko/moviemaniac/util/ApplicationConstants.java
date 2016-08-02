bdb461276f9d5273c1d1f5379169538c956d4088
Thomas Kioko <kiokotomas@gmail.com>
2016-08-03 01:12:08 +0300

    Remove unused constants

--- MovieManiac/app/src/main/java/com/thomaskioko/moviemaniac/util/ApplicationConstants.java
+++ MovieManiac/app/src/main/java/com/thomaskioko/moviemaniac/util/ApplicationConstants.java
@@ -80,55 +80,10 @@ public class ApplicationConstants {
     public static final int COL_MOVIE_RELEASE_DATE = 9;
 
 
-    // Prefs
-    public static final String prefName = "MOVIZ_PREF";
-
-    public static final String PREF_MOVIE_LIST_TYPE = "PREF_MOVIE_LIST_TYPE";
+    /**
+     * Movie types
+     */
     public static final String PREF_MOVIE_LIST_POPULAR = "popular";
     public static final String PREF_MOVIE_LIST_TOP_RATED = "top_rated";
-    public static final String PREF_MOVIE_LIST_NOW_PLAYING = "now_playing";
-    public static final String PREF_MOVIE_LIST_UPCOMING = "upcoming";
     public static final String PREF_MOVIE_LIST_FAVORITES = "favorites";
-    public static final String PREF_MOVIE_LIST_SEARCH = "search";
-
-    //Callbacks
-    public static final String CALLBACK_FETCH_MOVIES_WITH_PAGE = "CALLBACK_FETCH_MOVIES_WITH_PAGE";
-
-    public static final String CALLBACK_IS_FAVORITE = "CALLBACK_IS_FAVORITE";
-    public static final String CALLBACK_DELETE_FAVORITE = "CALLBACK_DELETE_FAVORITE";
-    public static final String CALLBACK_ADD_FAVORITE = "CALLBACK_ADD_FAVORITE";
-    public static final String CALLBACK_QUERY_FAVORITE_LIST = "CALLBACK_QUERY_FAVORITE_LIST";
-
-    public static final String CALLBACK_MOVIE_BUNDLE = "CALLBACK_MOVIE_BUNDLE";
-    public static final String CALLBACK_REFRESH_FAVORITES = "CALLBACK_REFRESH_FAVORITES";
-    public static final String CALLBACK_VIEW_SEARCH_RESULTS = "CALLBACK_VIEW_SEARCH_RESULTS";
-
-    public static final String CALLBACK_FETCH_POPULAR = "CALLBACK_FETCH_POPULAR";
-
-    //MoviesListFragment
-    public static final String KEY_MOVIE_OBJECTS = "KEY_MOVIE_OBJECTS";
-    public static final String KEY_MOVIE_LIST_TYPE = "KEY_MOVIE_LIST_TYPE";
-    public static final String KEY_CURRENT_PAGE = "KEY_CURRENT_PAGE";
-    public static final String KEY_CURRENT_QUERY_STRING = "KEY_CURRENT_QUERY_STRING";
-    public static final String KEY_LIST_POSITION = "KEY_LIST_POSITION";
-    public static final String KEY_FIRST_FETCH = "KEY_FIRST_FETCH";
-
-    //DetailsFragment
-    public static final String KEY_MOVIE_OBJECT = "KEY_MOVIE_OBJECT";
-    public static final String KEY_VIDEO_OBJECTS = "KEY_VIDEO_OBJECTS";
-    public static final String KEY_MOVIE_TITLE = "KEY_MOVIE_TITLE";
-    public static final String KEY_MOVIE_ID = "KEY_MOVIE_ID";
-    public static final String KEY_TMDB_RAW_REVIEW_OBJECT = "KEY_TMDB_RAW_REVIEW_OBJECT";
-    public static final String KEY_REVIEW_OBJECTS = "KEY_REVIEW_OBJECTS";
-    public static final String KEY_REVIEW_PAGING = "KEY_REVIEW_PAGING";
-
-    //Database Related
-    public static final String TASK_IS_FAVORITE = "TASK_IS_FAVORITE";
-    public static final String TASK_DELETE_FAVORITE = "TASK_DELETE_FAVORITE";
-    public static final String TASK_ADD_FAVORITE = "TASK_ADD_FAVORITE";
-    public static final String TASK_QUERY_FAVORITE_LIST = "TASK_QUERY_FAVORITE_LIST";
-
-    //RecyclerViewWithEmptyView States
-    public static final String STATE_NO_NETWORK = "STATE_NO_NETWORK";
-    public static final String STATE_NO_FAV = "STATE_NO_FAV";
 }
