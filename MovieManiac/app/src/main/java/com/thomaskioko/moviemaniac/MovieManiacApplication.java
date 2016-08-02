1baf6fd3dcff83614f705d750586fbb502c56d77
Thomas Kioko <kiokotomas@gmail.com>
2016-08-02 23:33:15 +0300

    Add shared preference method

--- MovieManiac/app/src/main/java/com/thomaskioko/moviemaniac/MovieManiacApplication.java
+++ MovieManiac/app/src/main/java/com/thomaskioko/moviemaniac/MovieManiacApplication.java
@@ -1,10 +1,13 @@
 package com.thomaskioko.moviemaniac;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
 
 import com.thomaskioko.moviemaniac.api.TmdbApiClient;
 import com.thomaskioko.moviemaniac.model.Result;
 import com.thomaskioko.moviemaniac.util.ApplicationConstants;
+import com.thomaskioko.moviemaniac.util.Utils;
 
 /**
  * Application class.
@@ -15,11 +18,21 @@ public class MovieManiacApplication extends Application {
 
     private static TmdbApiClient tmdbApiClient = new TmdbApiClient();
     public static Result result;
+    public static SharedPreferences mSharedPreferences;
+    public static String savedMovieListType;
+    public static boolean isTwoPane;
 
     @Override
     public void onCreate() {
         super.onCreate();
         tmdbApiClient.setIsDebug(ApplicationConstants.DEBUG);
+
+        mSharedPreferences = getSharedPreferences(ApplicationConstants.prefName, Context.MODE_PRIVATE);
+        savedMovieListType = Utils.readSharedPreferences(ApplicationConstants.PREF_MOVIE_LIST_TYPE, ApplicationConstants.PREF_MOVIE_LIST_POPULAR);
+
+        //Switch to popular if in search state
+        if (ApplicationConstants.PREF_MOVIE_LIST_SEARCH.equals(savedMovieListType))
+            savedMovieListType = ApplicationConstants.PREF_MOVIE_LIST_POPULAR;
     }
 
     /**
