8a15dd387a9cb30248d484a785d42b95de7ceedd
Thomas Kioko <kiokotomas@gmail.com>
2016-08-03 22:56:08 +0300

    Implement callbacks and save data in onSaveInstance

--- MovieManiac/app/src/main/java/com/thomaskioko/moviemaniac/ui/MovieListActivity.java
+++ MovieManiac/app/src/main/java/com/thomaskioko/moviemaniac/ui/MovieListActivity.java
@@ -2,10 +2,17 @@ package com.thomaskioko.moviemaniac.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
 import com.thomaskioko.moviemaniac.MovieManiacApplication;
 import com.thomaskioko.moviemaniac.R;
+import com.thomaskioko.moviemaniac.interfaces.MovieDetailCallback;
+import com.thomaskioko.moviemaniac.interfaces.MovieCallback;
+import com.thomaskioko.moviemaniac.model.Result;
+import com.thomaskioko.moviemaniac.ui.fragments.MovieDetailFragment;
 import com.thomaskioko.moviemaniac.util.ApplicationConstants;
 import com.thomaskioko.moviemaniac.util.SharedPreferenceManager;
+
+import java.util.ArrayList;
 
 import butterknife.ButterKnife;
 
@@ -17,9 +24,10 @@ import butterknife.ButterKnife;
  * item details. On tablets, the activity presents the list of items and
  * item details side-by-side using two vertical panes.
  */
-public class MovieListActivity extends AppCompatActivity  {
+public class MovieListActivity extends AppCompatActivity implements MovieCallback, MovieDetailCallback {
 
-    public static boolean twoPane = false;
+    public static boolean isTwoPane = false;
+    public static MovieCallback mCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +35,8 @@ public class MovieListActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_movie_list);
         ButterKnife.bind(this);
 
-        twoPane = findViewById(R.id.movie_details_container) != null;
-        MovieManiacApplication.isTwoPane = twoPane;
+        isTwoPane = findViewById(R.id.movie_details_container) != null;
+        MovieManiacApplication.isTwoPane = isTwoPane;
 
         setUpToolBarTitle();
 
@@ -43,15 +51,44 @@ public class MovieListActivity extends AppCompatActivity  {
         SharedPreferenceManager sharedPreferences = new SharedPreferenceManager(getApplicationContext());
         switch (sharedPreferences.getMovieType()) {
             case ApplicationConstants.PREF_MOVIE_LIST_POPULAR:
-                setTitle("Popular");
+                setTitle(getString(R.string.menu_action_popular));
                 break;
             case ApplicationConstants.PREF_MOVIE_LIST_TOP_RATED:
-                setTitle("Top Rated");
+                setTitle(getString(R.string.menu_action_top_rated));
                 break;
             case ApplicationConstants.PREF_MOVIE_LIST_FAVORITES:
-                setTitle("Favorites");
+                setTitle(getString(R.string.menu_action_favorites));
                 break;
         }
     }
 
+    /**
+     * Helper method to instantiate {@link #mCallback}
+     * @param callback {@link MovieCallback}
+     */
+    public static void setCallback(MovieCallback callback) {
+        mCallback = callback;
+    }
+
+    @Override
+    public void CallbackRequest(String requestType, String bundleData) {
+        if (requestType.equals(ApplicationConstants.CALLBACK_REFRESH_FAVORITES)) {
+            mCallback.CallbackRequest(ApplicationConstants.CALLBACK_REFRESH_FAVORITES, "");
+        }
+    }
+
+    @Override
+    public void CallbackRequest(String requestType, ArrayList<Result> resultArrayList) {
+    }
+
+    @Override
+    public void CallbackRequest(String request, Bundle bundle) {
+
+        MovieDetailFragment fragment = new MovieDetailFragment();
+        fragment.setArguments(bundle);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.movie_details_container, fragment)
+                .commit();
+    }
 }
