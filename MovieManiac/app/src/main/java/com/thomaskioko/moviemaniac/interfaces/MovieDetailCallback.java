f2c9c5efbe652e099b316aaabfe16f4baa8dae6e
Thomas Kioko <kiokotomas@gmail.com>
2016-08-03 22:53:31 +0300

    Create callback interface classes

---
+++ MovieManiac/app/src/main/java/com/thomaskioko/moviemaniac/interfaces/MovieDetailCallback.java
@@ -0,0 +1,15 @@
+package com.thomaskioko.moviemaniac.interfaces;
+
+import android.os.Bundle;
+
+/**
+ * Movie Detail callback
+ *
+ * @author Thomas Kioko
+ */
+public interface MovieDetailCallback {
+    void CallbackRequest(String request, Bundle bundle);
+}
+
+
+
