5cffcfc0c5f525793bbdc92a6f6e8f31333943e8
Thomas Kioko <kiokotomas@gmail.com>
2016-08-27 18:56:44 +0300

    Refactor class,
    Move class to utils package

--- xyzreader/XYZReader/src/main/java/com/example/xyzreader/ui/util/DynamicHeightNetworkImageView.java
+++ xyzreader/XYZReader/src/main/java/com/example/xyzreader/ui/util/DynamicHeightNetworkImageView.java
@@ -1,11 +1,10 @@
-package com.example.xyzreader.ui;
+package com.example.xyzreader.ui.util;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 
-import com.android.volley.toolbox.NetworkImageView;
-
-public class DynamicHeightNetworkImageView extends NetworkImageView {
+public class DynamicHeightNetworkImageView extends ImageView {
     private float mAspectRatio = 1.5f;
 
     public DynamicHeightNetworkImageView(Context context) {
@@ -20,15 +19,15 @@ public class DynamicHeightNetworkImageView extends NetworkImageView {
         super(context, attrs, defStyle);
     }
 
-    public void setAspectRatio(float aspectRatio) {
-        mAspectRatio = aspectRatio;
-        requestLayout();
-    }
-
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int measuredWidth = getMeasuredWidth();
         setMeasuredDimension(measuredWidth, (int) (measuredWidth / mAspectRatio));
+    }
+
+    public void setAspectRatio(float aspectRatio) {
+        mAspectRatio = aspectRatio;
+        requestLayout();
     }
 }
